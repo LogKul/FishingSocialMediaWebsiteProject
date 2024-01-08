cf2819ceb43d4669279b048bc81f56da8378c44d
Alex Gonzales <gonzales.alexzander18@gmail.com>
2024-01-08 02:19:01 -0700

    made the post its own component

--- ReactApp/fish-media/src/pages/home.page.jsx
+++ ReactApp/fish-media/src/pages/home.page.jsx
@@ -4,6 +4,7 @@ import './../Styles/Home.css';
 import testImage from './../Images/RainbowTroutTest.jpg'
 import profilePic from './../Images/ProfilePic.jpg'
 
+import Post from '../Components/post.jsx';
 
 function HomePage() {
 
@@ -12,27 +13,8 @@ function HomePage() {
   }
 
   return (
-    <div className="App">
-      <div className="post">
-        <div className="posterProfile">
-          <img src={profilePic} alt="" className="posterPic"/>
-          <p className="poster">John Boi</p>
-        </div>
-        <img src={testImage} alt="postImage" className="postImage" />
-        <div className="postStats">
-          <p className="statTitle">Species</p>
-          <p className="statTitle">Weight</p>
-          <p className="statTitle">Length</p>
-
-          <p className="stat">RainbowTrout</p>
-          <p className="stat">12lbs</p>
-          <p className="stat">40in</p>
-        </div>
-        <hr />
-        <p className="caption">
-        This fish is already dead, he just doesn't know it yet.
-        </p>
-      </div>
+    <div>
+      <Post/>
     </div>
   );
 }
